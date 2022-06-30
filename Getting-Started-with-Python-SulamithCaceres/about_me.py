c9546eaf4872c9d9e513bda9c578cb79af7ef880
Sulamith Caceres <sulamithcaceres@sulamiths-mbp.mynetworksettings.com>
2022-06-30 11:21:31 -0700

    adding 'getting started with python' activity

---
+++ Getting-Started-with-Python-SulamithCaceres/about_me.py
@@ -0,0 +1,21 @@
+full_name = "Sulamith K. Caceres Ranilla"
+preferred_name = "Suli"
+age = 19
+python_experience = True
+
+hobbies = ["painting","reading","writing"]
+
+fav_things = {
+  "movie": "Sorry to Bother You",
+  "food": "ceviche",
+  "hobby": hobbies[0],
+  "anime": "Jojo's Bizarre Adventure",
+}
+
+print(full_name, preferred_name, age, python_experience)
+print()
+print(hobbies)
+print()
+print(fav_things)
+
+all_vars = dict(vars())
